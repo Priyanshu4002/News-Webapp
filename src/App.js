1c529671a03813636c1ed402e8c20d2c32076628
Priyanshu Shrivastava <125429914+Priyanshu4002@users.noreply.github.com>
2023-08-01 22:08:19 -0700

    Initial commit for third project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+//import {
+  //BrowserRouter as Router,
+  //Switch,
+  //Route,
+//} from "react-router-dom";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+       {/* <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/"><News  pageSize={6} country={"us"} category={"general"}/></Route>
+            <Route path="/business"><News  pageSize={6} country={"us"} category={"business"}/></Route>
+            <Route path="/entertainment"><News  pageSize={6} country={"us"} category={"entertainment"}/></Route>
+            <Route path="/general"><News  pageSize={6} country={"us"} category={"general"}/></Route>
+            <Route path="/health"><News  pageSize={6} country={"us"} category={"health"}/></Route>
+            <Route path="/science"><News  pageSize={6} country={"us"} category={"science"}/></Route>
+            <Route path="/sports"><News  pageSize={6} country={"us"} category={"sports"}/></Route>
+            <Route path="/technology"><News  pageSize={6} country={"us"} category={"technology"}/></Route>
+         </Switch>
+        </Router> */}
+        < Navbar />
+        <News pageSize={6}  country={"us"} category={"business"}/>
+      </div>
+    )
+  }
+}
